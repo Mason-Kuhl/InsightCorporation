5eb95d91e6069cb8be98df06ddc5330c545c9769
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-10-11 00:27:03 -0500

    Add project files.

---
+++ InsightCorp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InsightCorp.Startup))]
+namespace InsightCorp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
