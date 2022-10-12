bb5cd8b12173904a5d08a05d50c4d9d8332250d8
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-10-12 01:59:36 -0500

    made executive's employee list

---
+++ InsightCorp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using InsightCorp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsightCorp.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public UserController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var users = _context.Users.ToList();
+
+            if (User.IsInRole("Executive"))
+            {
+                return View("Index", users);
+            }
+
+            return View();
+        }
+    }
+}
